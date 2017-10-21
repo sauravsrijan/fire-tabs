2c0b06597c820d3f444dda640bf9d421a206eae1
Srijan <sauravsrijan@gmail.com>
2017-10-22 01:24:19 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+This script will open up a browsing session with the urls provided in the script.
+
+Here, I am opening 3 websites.
+-FACEBOOK
+-GOOGLE
+-GMAIL
---
+++ tabs.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+url1 = "https://google.com"
+url2 = "https://www.facebook.com"
+url3 = "https://www.gmail.com"
+
+webbrowser.open_new(url1)
+webbrowser.open_new_tab(url2)
+webbrowser.open_new_tab(url3)
